936089786511a4213acd05673e8bfd8e7923de53
unknown <sanchitmishra88@gmail.com>
2020-08-16 21:02:26 +0530

    Token Expiration Time

--- src/main/java/com/kanban/demo/security/SecurityConstants.java
+++ src/main/java/com/kanban/demo/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; //300 Seconds
+    public static final long EXPIRATION_TIME = 3000000; //milliseconds
 }
 
